4f530e4bb754279659e84c85784fbfb541a2b99e
ybw@qq.com <ybw@qq.com>
2019-07-22 13:38:25 +0800

    模板及静态文件配置

--- Djangoshop/Djangoshop/settings.py
+++ Djangoshop/Djangoshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Shopapp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
+
+
+
+# 上传图片用的模块
+CKEDITOR_UPLOAD_PATH = 'static/upload'
+CKEDITOR_IMAGE_BACKED = 'pillow'
+
+# 当前项目的静态文件目录
+# STATIC_ROOT =  os.path.join(BASE_DIR,'static')
+
+
+
+
+
+
